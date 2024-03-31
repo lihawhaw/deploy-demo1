ba7aceffbded12ce00344a8bc0a7fc16e87b1f18
lihawhaw <lihawhaw@163.com>
2024-03-31 15:12:56 +0800

    test

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { port } from '../adp.config.js'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log('app-port', port);
   await app.listen(port);
 }
 bootstrap();
